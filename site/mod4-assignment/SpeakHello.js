3efe25a1e74fbd97200029b0663591ac6320982d
maanasa-99 <76205583+maanasa-99@users.noreply.github.com>
2020-12-21 18:32:27 +0530

    Assignments

---
+++ site/mod4-assignment/SpeakHello.js
@@ -0,0 +1,9 @@
+(function(window){
+var hellospeaker = {};
+var speakWord = "Hello";
+hellospeaker.speak=function(name) {
+  console.log(speakWord + " " + name);
+}
+window.hellospeaker = hellospeaker;
+})(window);
+
